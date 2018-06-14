23174661149d0d753d7d97a5026a52ab8074b8fb
Roy Vanegas <roy@thecodeeducators.com>
2018-06-14 13:55:21 -0400

    Add hoisting example

---
+++ es6-examples/js/02--hoisting.js
@@ -0,0 +1,10 @@
+// “i” is accessible but “undefined.”
+console.log('The value of i is “' + i + '”');
+
+// “i” is hoisted to the top of this scope, but initialized below.
+for (var i = 0; i < 2; i++) {
+    console.log('The value of i in the loop is “' + i + '”');
+}
+
+// “i” is available and holds the last value calculated in the for loop
+console.log('The value of i is “' + i + '”');
