16b417d3e81e00197a536cc828fc20b622609266
Roy Vanegas <roy@thecodeeducators.com>
2018-06-15 14:32:47 -0400

    Add multiline strings example

---
+++ es6-examples/js/05--strings--multiline.js
@@ -0,0 +1,8 @@
+// String: Multiline with newline characters
+
+let message = `
+Eagle in Your Mind\nappears on
+Music Has the Right to Children\nby
+Boards of Canada.`.trim();
+
+console.log(message);
