0d846fb8105a01fc0dd6c36d2598d1340c7f23d0
Roy Vanegas <roy@thecodeeducators.com>
2018-06-15 12:08:12 -0400

    Add example of the includes method

---
+++ es6-examples/js/05--strings--includes.js
@@ -0,0 +1,12 @@
+// Strings: includes method
+let animal = 'sheep';
+
+// ES5
+console.log(animal.indexOf("he"));
+console.log(animal.indexOf("ee"));
+console.log(animal.indexOf("they"));
+
+// ES6
+console.log(animal.includes("he"));
+console.log(animal.includes("ee", 2));
+console.log(animal.includes("they"));
