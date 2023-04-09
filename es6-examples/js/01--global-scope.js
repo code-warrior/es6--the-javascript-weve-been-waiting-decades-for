9371d506609f1bcc8d26619cd036fec628959f64
Roy Vanegas <roy@thecodeeducators.com>
2023-04-09 15:25:39 -0400

    Add line numbers to output

--- es6-examples/js/01--global-scope.js
+++ es6-examples/js/01--global-scope.js
@@ -1,10 +1,10 @@
 var globalVariable = 'I’m in the global scope';
 let notGlobalVariable = 'I’m not in the global scope';
 
-console.log('The value of globalVariable is “' + window.globalVariable + '”');
-console.log('Is globalVariable in window? “' + ('globalVariable' in window) + '”');
-console.log('Are globalVariable and window.globalVariable equal? “' + (globalVariable === window.globalVariable) + '”');
+console.log('4. The value of globalVariable is “' + window.globalVariable + '”');
+console.log('5. Is globalVariable in window? “' + ('globalVariable' in window) + '”');
+console.log('6. Are globalVariable and window.globalVariable equal? “' + (globalVariable === window.globalVariable) + '”');
 
-console.log('The value of notGlobalVariable is “' + window.notGlobalVariable + '”');
-console.log('Is notGlobalVariable in window? “' + ('notGlobalVariable' in window) + '”');
-console.log('Are notGlobalVariable and window.notGlobalVariable equal? “' + (notGlobalVariable === window.notGlobalVariable) + '”');
+console.log('8. The value of notGlobalVariable is “' + window.notGlobalVariable + '”');
+console.log('9. Is notGlobalVariable in window? “' + ('notGlobalVariable' in window) + '”');
+console.log('10. Are notGlobalVariable and window.notGlobalVariable equal? “' + (notGlobalVariable === window.notGlobalVariable) + '”');
