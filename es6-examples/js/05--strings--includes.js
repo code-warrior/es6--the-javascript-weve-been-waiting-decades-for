7124857512cc59d5ca8f9ec084a0f89c54ee99bb
Roy Vanegas <roy@thecodeeducators.com>
2023-04-09 21:37:27 -0400

    Add comment

--- es6-examples/js/05--strings--includes.js
+++ es6-examples/js/05--strings--includes.js
@@ -7,6 +7,6 @@ console.log(animal.indexOf(`ee`));
 console.log(animal.indexOf(`they`));
 
 // ES6
-console.log(animal.includes("ee", 2));
 console.log(animal.includes(`he`));
+console.log(animal.includes(`ee`, 2)); // Starting at index 2
 console.log(animal.includes(`they`));
